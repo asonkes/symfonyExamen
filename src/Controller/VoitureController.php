387d6da75f824887b532583880e9dc56a78181b5
asonkes <audrey.sonkes@gmail.com>
2023-11-14 22:13:21 +0100

    fin du projet

--- src/Controller/VoitureController.php
+++ src/Controller/VoitureController.php
@@ -3,10 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Voiture;
+use App\Form\AnnonceType;
 use App\Repository\VoitureRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class VoitureController extends AbstractController
 {
@@ -35,6 +39,51 @@ class VoitureController extends AbstractController
         return $this->render('voiture/show.html.twig', [
             'voiture' => $voiture,
             'images' => $images
+        ]);
+    }
+
+    #[Route("/vente/{id}/new", name: "vente_new")]
+    public function new(Request $request, EntityManagerInterface $manager, Voiture $voiture): Response
+    {
+        // Sert à créer une nouvelle voiture
+        $voiture = new Voiture();
+        // 
+        $form = $this->createForm(AnnonceType::class, $voiture);
+        $form->handleRequest($request);
+
+        /**
+         * C'est ici qu'on gère l'ajout ou la suppression de l'image de la voiture...
+         */
+        if ($form->isSubmitted() && $form->isValid()) {
+            //On récupère les datas de l'image
+            $file = $form["image"]->getData();
+            if (!empty($file)) {
+                $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = transliterator_transliterate('Any-Latin;Latin-ASCII; [^A-Za-z0-9] remove; Lower()', $originalFilename);
+                $newFileName = $safeFilename . '-' . uniqid() . '.' . $file->guessExtension();
+
+                try {
+                    $file->move(
+                        $this->getParameter("uploads_directory"),
+                        $newFileName
+                    );
+                } catch (FileException $error) {
+                    return $error->getMessage();
+                }
+
+                $voiture->setImage("images/" . $newFileName);
+            } else {
+                $voiture->setImage(null);
+            }
+
+            $manager->persist($voiture);
+            $manager->flush();
+
+            return $this->redirectToRoute("vente");
+        }
+        return $this->render('voiture/new.html.twig', [
+            // On crée la vue du formulaire
+            "form" => $form->createView()
         ]);
     }
 }
--- src/Form/AnnonceType.php
+++ src/Form/AnnonceType.php
@@ -6,25 +6,46 @@ use App\Entity\Voiture;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class AnnonceType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('marque')
-            ->add('modele')
-            ->add('image')
-            ->add('km')
-            ->add('prix')
-            ->add('nbproprio')
-            ->add('cylindree')
-            ->add('puissance')
-            ->add('carburant')
-            ->add('transmission')
-            ->add('description')
-            ->add('texte')
-            ->add('circ');
+            ->add('marque', TextType::class, $this->getConfiguration("Marque : "))
+            ->add('modele', TextType::class, $this->getConfiguration("Modèle : "))
+            ->add('image', FileType::class, $this->getConfiguration("Image de la Voiture : "))
+            ->add('km', NumberType::class, $this->getConfiguration("Kilométrage de la Voiture : "))
+            ->add('prix', MoneyType::class, $this->getConfiguration("Prix de la voiture :"))
+            ->add('nbproprio', NumberType::class, $this->getConfiguration("Nombre de propriétaires :"))
+            ->add('cylindree', NumberType::class, $this->getConfiguration("Cylindrée de la voiture :"))
+            ->add('puissance', NumberType::class, $this->getConfiguration("Puissance de la voiture :"))
+
+            ->add('carburant', ChoiceType::class, [
+                'choices' => [
+                    'Essence' => 'Essence',
+                    'Diesel' => 'Diesel',
+                    'Electrique' => 'Electrique'
+                ]
+            ])
+
+            ->add('transmission', choiceType::class, [
+                'choices' => [
+                    'Automatique' => 'Automatique',
+                    'Manuelle' => 'Manuelle',
+                ]
+            ])
+            ->add('description', TextareaType::class, $this->getConfiguration("Description de la voiture :"))
+            ->add('texte', TextareaType::class, $this->getConfiguration("Si vous avez une information à rajouter : "))
+            ->add('circ', NumberType::class, $this->getConfiguration("Type de circulation :"));
     }
 
     public function configureOptions(OptionsResolver $resolver): void
@@ -32,5 +53,13 @@ class AnnonceType extends AbstractType
         $resolver->setDefaults([
             'data_class' => Voiture::class,
         ]);
+    }
+
+    private function getConfiguration($label): array
+    {
+        return [
+            'label' => $label,
+            // Autres options éventuelles...
+        ];
     }
 }
